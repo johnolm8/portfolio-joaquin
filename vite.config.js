a09dd98b78e8522321efa597b908e6440ed46424
johnolm8 <johnolm8@gmail.com>
2024-06-14 22:51:30 +0000

    ready

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  base: command === 'build' ? '/Portfolio-Joaquin/' : '/', // Replace 'my-portfolio' with your repo name
+}));
